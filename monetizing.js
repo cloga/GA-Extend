5e12e78e0e4c17da0c3d49cadcba5edb9abcbe1d
Cloga <cloga0216@gmail.com>
2013-03-06 14:52:22 +0800

    creat

---
+++ monetizing.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+function getOrderID(){
+// generate a random order id
+	var randomnumber = Math.floor(Math.random()*1000);
+	var current = new Date();
+	var month = current.getMonth()+1;
+	var timeStamp = current.getFullYear() +month +current.getDate() + '-' +current.getHours() + current.getMinutes()+ current.getSeconds() +'-' + randomnumber;
+	return(timeStamp);
+}
+function trackEng(name,value=0){
+	// generate pseudo e-commerce trans
+	var orderNum = getOrderID();
+	_gaq.push(['_addTrans', orderNum, '', String(value), '', '','','','']);
+	_gaq.push(['_addItem', orderNum, name, '', '', String(value), '1']);
+	_gaq.push(['_trackTrans']);
+	_gaq.push(['_clearTrans']);
+}
+</script>
